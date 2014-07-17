b8c29d60c9cd9078bad3fa45b7a29460126cab4d
Colin Jackson <colin.s.jackson90@gmail.com>
2014-07-17 04:45:59 +0100

    Add selected image logic programmatically, and add placeholder view controllers in Storyboard

---
+++ AMS Express/AMSTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  AMSTabBarController.h
+//  AMS Express
+//
+//  Created by Colin on 7/17/14.
+//  Copyright (c) 2014 Alpert Medical School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMSTabBarController : UITabBarController
+
+@end
---
+++ AMS Express/AMSTabBarController.m
@@ -0,0 +1,67 @@
+//
+//  AMSTabBarController.m
+//  AMS Express
+//
+//  Created by Colin on 7/17/14.
+//  Copyright (c) 2014 Alpert Medical School. All rights reserved.
+//
+
+#import "AMSTabBarController.h"
+
+@interface AMSTabBarController ()
+
+@end
+
+@implementation AMSTabBarController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    [self customizeTabBarItems];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (void)customizeTabBarItems
+{
+    NSArray *imageNames = @[@"NotesTabSelected", @"EchoTabSelected", @"CalendarTabSelected", @"SettingsTabSelected"];
+    NSMutableArray *images = [[NSMutableArray alloc] init];
+    for (NSString *imageName in imageNames) {
+        UIImage *image = [UIImage imageNamed:imageName];
+        [images addObject:image];
+    }
+    
+    NSUInteger i = 0;
+    for (UIViewController *vc in self.viewControllers) {
+        UITabBarItem *tabBarItem = vc.tabBarItem;
+        [tabBarItem setSelectedImage:[images objectAtIndex:i]];
+        i = i + 1;
+    }
+}
+
+@end
